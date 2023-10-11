c3d6b7f48606efd1c248f0de2ccb6bbb805906c7
zacharycallahan <zachcallahan90@gmail.com>
2023-10-11 02:28:14 -0400

    Updated Card headers

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 const Card = ({title, children}) => {
     return (
         <div className="bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 shadow-md hover:shadow-xl w-full rounded-xl overflow-hidden transform transition-all duration-500 hover:scale-105 hover:-rotate-1">
-            <div className="bg-gradient-to-br from-blue-500 to-indigo-600 dark:from-indigo-700 dark:to-indigo-900 p-5">
-                <h3 className="text-xl font-bold text-white">{title}</h3>
-            </div>
+                <h3 className="text-xl p-6 border-b-2 border-gray-300 dark:border-gray-700 text-center font-bold text-white">{title}</h3>
+            
             <div className="p-6 flex flex-wrap gap-6 items-center justify-center dark:text-gray-300">
                 {children}
             </div>
